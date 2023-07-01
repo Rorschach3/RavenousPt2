b72a3f7ade7aec2fd976237289d43d4c0baa7bb3
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-06-30 22:48:50 -0700

    navbar

--- src/components/App/Mainpage.js
+++ src/components/App/Mainpage.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import './mainpage.css';
 
 function Mainpage() {
     return (
-        <div>
-            <h1>Mainpage</h1>
-        </div>
+    <div id='navBar' className='Container'>
+        <nav className="navbar navbar-light bg-light justify-content-between">
+        <a className="navbar-brand">Ravenous</a>
+        <form className="form-inline">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search"></input>
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+        </form>
+        </nav>
+    </div>
     )
 }
 
