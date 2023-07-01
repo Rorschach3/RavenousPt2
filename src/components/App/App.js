65368d215b6d4a24e2e47417180aca910649583e
Rorschach3 <daniel_fhernandez@yahoo.com>
2023-06-30 20:52:47 -0700

    ohh my God finally

---
+++ src/components/App/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
+import Business from '../Business/Business';
+import Mainpage from '../App/Mainpage';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Mainpage />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
